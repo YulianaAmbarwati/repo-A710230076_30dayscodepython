9ffa94b734c48fb07c6b72934281f7e1ede36549
YulianaAmbarwati <ambarwatiyuliana481@gmail.com>
2024-06-11 20:20:13 +0700

    Fungsi

---
+++ Day 2/Fungsi.py
@@ -0,0 +1,11 @@
+kota, provinsi = 'Lamongan', 'Jawa Timur'
+
+def hello ():
+  provinsi = 'Jawa Barat'
+  print(kota, provinsi)
+
+print('[PANGGIL FUNGSI hello()]')
+hello()
+
+print('\n[SECARA LANGSUNG]')
+print(kota, provinsi)
