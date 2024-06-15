d0eac076200f5225ba77d38e492f8924c541b00d
YulianaAmbarwati <ambarwatiyuliana481@gmail.com>
2024-06-15 18:11:58 +0700

    Tuple

---
+++ Day 6/Tuple.py
@@ -0,0 +1,14 @@
+# Membuat tuple dengan beberapa elemen
+my_tuple = (1, 2, 3, "a", "b", "c")
+
+# Menampilkan tuple
+print("Tuple:", my_tuple)
+
+# Mengakses elemen pertama
+print("Elemen pertama:", my_tuple[0])
+
+# Mengakses elemen terakhir
+print("Elemen terakhir:", my_tuple[-1])
+
+# Mengakses elemen dari indeks 1 hingga 3 (tidak termasuk 3)
+print("Slice [1:3]:", my_tuple[1:3])
