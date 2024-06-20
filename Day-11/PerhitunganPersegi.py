c98b5cb375b6f35a73bbac5434b702cdcff9ff4c
YulianaAmbarwati <ambarwatiyuliana481@gmail.com>
2024-06-20 21:35:56 +0700

    PerhitunganPersegi

---
+++ Day-11/PerhitunganPersegi.py
@@ -0,0 +1,14 @@
+print("Menghitung luas dan keliling persegi")
+
+#mengambil inputan panjang
+panjang = input("Masukkan nilai panjang :")
+
+#mengambil inputan lebar
+lebar = input("Masukkan nilai lebar :")
+
+#menghitung luas dan keliling
+luas = int(panjang) * int(lebar)
+keliling = 2 * (int(panjang) * int(lebar))
+
+#menampilkan hasil
+print("luas persegi adalah", luas, "sedangkan kelilingnya adalah", keliling)
