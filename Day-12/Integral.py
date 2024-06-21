146347b2737214b5e060d5293c6617c07cfb4ae9
YulianaAmbarwati <ambarwatiyuliana481@gmail.com>
2024-06-21 18:34:40 +0700

    Integral

---
+++ Day-12/Integral.py
@@ -0,0 +1,21 @@
+# Deklarasi variabel integral
+x = 15
+y = 4
+
+# Operasi aritmatika dasar
+penjumlahan = x + y
+pengurangan = x - y
+perkalian = x * y
+pembagian = x / y # Hasilnya adalah float
+modulus = x % y
+pangkat = x ** y
+pembagian_bulat = x // y # hasilnya adalah integer
+
+# Cetak hasil
+print("Penjumlahan:", penjumlahan)
+print("Pengurangan:", pengurangan)
+print("Perkalian:", perkalian)
+print("Pembagian:", pembagian)
+print("Modulus:", modulus)
+print("Pangkat:", pangkat)
+print("Pembagian Bulat:", pembagian_bulat)
