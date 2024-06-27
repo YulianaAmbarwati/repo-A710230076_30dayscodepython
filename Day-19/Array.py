f49fd1d154d0cd7d32d32556bf1044ea6c3553e5
YulianaAmbarwati <ambarwatiyuliana481@gmail.com>
2024-06-28 06:49:14 +0700

    Array

---
+++ Day-19/Array.py
@@ -0,0 +1,22 @@
+# Membuat daftar
+daftar_buah = ["apel", "jeruk", "mangga", "pisang"]
+
+# Mengakses elemen dalam daftar
+print(daftar_buah[0])  
+print(daftar_buah[2])  
+
+# Menambahkan elemen ke daftar
+daftar_buah.append("anggur")
+print(daftar_buah)  
+
+# Menambahkan elemen di posisi tertentu
+daftar_buah.insert(1, "kiwi")
+print(daftar_buah)  
+
+# Menghapus elemen dari daftar
+daftar_buah.remove("mangga")
+print(daftar_buah)  
+
+# Menghapus elemen berdasarkan indeks
+del daftar_buah[0]
+print(daftar_buah)  
